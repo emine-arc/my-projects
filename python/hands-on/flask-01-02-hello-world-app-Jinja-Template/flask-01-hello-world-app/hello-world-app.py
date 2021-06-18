8f1358a28ebcd528bf63e917f04ffcbafd70d514
emine-arc <emineulu.90@gmail.com>
2021-06-19 01:57:59 +0300

    flask 01-02 added

--- python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -3,21 +3,28 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def head():
-    return "I like Flask"
+def hello():
+    return 'Hello World!'
 
 @app.route('/second')
 def second():
-    return "This is the second page"
+    return 'Bize Her Yer Malatya'
 
-@app.route('/third/subthird')
+@app.route('/third/subthird')  
 def third():
-    return "This is the subpage of third page"
+    return 'This is the subpage of third page'
 
-@app.route('/forth/<string:id>')
+@app.route('/forth/<string:id>')  
 def forth(id):
-    return f'Id of this page is {id}'
+    return f'Id number of this page is{id}'
 
-if __name__=="__main__":
+
+if __name__== '__main__':
+    #app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
-    # app.run(debug=True)
+
+
+
+
+
+
